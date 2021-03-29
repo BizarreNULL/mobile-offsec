c6cb77a787ae38f80c91469b30a6b676ee7c59b5
Jojo <jonas.uliana@passwd.com.br>
2021-03-28 22:58:55 -0300

    Updated to frida-server 14.2.14

--- installers/setup_frida_x86.sh
+++ installers/setup_frida_x86.sh
@@ -5,11 +5,11 @@
 # Project:     mobile-offsec
 # Repository:  https://github.com/BizarreNULL/mobile-offsec
 # License:     WTFPL
-# Description: Install frida-server on a shit way on remote
+# Description: Install frida-server on remove
 
-FRIDA_LAST_RELEASE_URL="https://github.com/frida/frida/releases/download/12.8.1/frida-server-12.8.1-android-x86.xz"
-ARCH=`adb shell getprop ro.product.cpu.abi`
-FRIDA_PID=`adb shell pgrep frida`
+FRIDA_LAST_RELEASE_URL="https://github.com/frida/frida/releases/download/14.2.14/frida-server-14.2.14-android-x86.xz"
+ARCH=$(adb shell getprop ro.product.cpu.abi)
+FRIDA_PID=$(adb shell pgrep frida)
 
 echo "[+] Checking current device architecture..."
 
@@ -21,13 +21,13 @@ then
    exit -1
 fi
 
-echo "[+] Downloading Frida Server..."
+echo "[+] Downloading frida-server..."
 wget $FRIDA_LAST_RELEASE_URL -O /tmp/frida.xz >/dev/null 2>&1
 unxz /tmp/frida.xz
 
 if [ ! -z "$FRIDA_PID" ] 
 then
-   echo "[+] Killing any Frida Server instances on Android device..."
+   echo "[+] Killing any frida-server instances on Android device..."
    adb shell "su 0 kill $FRIDA_PID"
 fi
 
@@ -35,5 +35,5 @@ echo "[+] Pushing binaries to remote..."
 adb push /tmp/frida /data/local/tmp
 echo "[+] Granting execute privileges..."
 adb shell "su 0 chmod 077 /data/local/tmp/frida"
-echo "[+] Starting Frida Server as root user..."
+echo "[+] Starting frida-server"
 adb shell "su 0 ./data/local/tmp/frida"
