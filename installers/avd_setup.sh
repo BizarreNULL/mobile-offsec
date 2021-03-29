4e38d36bd8bc488472d939beeab85b5e4a539370
Jojo <jonas.uliana@passwd.com.br>
2021-03-28 22:55:06 -0300

    Added cert setup for AVD with API 28

---
+++ installers/avd_setup.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# Copyright (C) 2021 6a6f6a6f <jonas.uliana@passwd.com.br>
+
+# Project:     mobile-offsec
+# Repository:  https://github.com/BizarreNULL/mobile-offsec
+# License:     WTFPL
+# Description: Install .CRT cert on AVD as system authority
+
+# Change the bellow values with you need
+ADB_DEVICE="emulator-5554"
+CERTIFICATE_PATH="../certificates/burp_suite_default.crt"
+
+if [ "$(adb -s "$ADB_DEVICE" shell getprop ro.build.version.sdk)" != "28" ];then
+    echo "[!] AVD image must be Android 9!"
+    exit 1
+fi
+
+FILENAME=$(openssl x509 -in "$CERTIFICATE_PATH" -hash -noout)
+FILENAME=$FILENAME".0"
+
+echo "[+] Generated certificate filename is $FILENAME"
+echo "[+] Creating certificate file..."
+
+openssl x509 -in "$CERTIFICATE_PATH" >> "$FILENAME"
+openssl x509 -in "$CERTIFICATE_PATH"  -text -fingerprint -noout >> "$FILENAME"
+
+echo "[+] Pushing to remote..."
+
+adb root
+sleep 5
+adb disable-verity
+adb remount
+adb -s "$ADB_DEVICE" push "$FILENAME" /sdcard
+adb -s "$ADB_DEVICE" shell "su 0 mv /sdcard/$FILENAME /system/etc/security/cacerts"
+adb -s "$ADB_DEVICE" shell "su 0 chmod 644 /system/etc/security/cacerts/$FILENAME"
+adb unroot
+sleep 5
+rm "$FILENAME" 2>/dev/null
+
+echo "[+] Done, check if the cert is visible in settings."
